f91a46292ef02bb40f0093b552f2a8132a9e319e
MaDLuck5 <matsslik5@gmail.com>
2024-05-23 00:24:13 +0200

    added docstrings

--- Assignment1/assignment1.py
+++ Assignment1/assignment1.py
@@ -1,10 +1,43 @@
 """
-this program calculates the average PHRED scores from FastQ files per base.
+This module provides functionality to calculate average PHRED scores from FastQ files.
+The script processes multiple FastQ files simultaneously, efficiently handling large datasets using Python's multiprocessing capabilities.
+The average PHRED scores are output either to the console or to CSV files for further analysis.
+The computational workload scales dynamically based on the number of available CPU cores.
 
+Features:
+- Processes multiple FastQ files simultaneously.
+- Efficiently handles large datasets using Python's multiprocessing capabilities.
+- Outputs average PHRED scores either to the console or to CSV files for further analysis.
+- Dynamically scales the computational workload based on the number of available CPU cores.
+
+Dependencies:
+- Python 3.11 or higher
+- NumPy library
+
+Usage:
+The script is executed from the command line with the following syntax:
+
+    python assignment1.py -n <number_of_cores> [-o] <fastq_file1> [<fastq_file2> ...]
+
+Parameters:
+- `-n` : Required. Specifies the number of CPU cores to use for processing.
+- `-o` : Optional. If set, the program outputs the results to CSV files named after the input files with an added '.output.csv' suffix.
+- `<fastq_files>` : Required. One or more paths to FastQ files that need to be processed.
+
+Output:
+- If `-o` is not specified, the average PHRED scores for each base position are printed to the console in the format "position,score".
+- If `-o` is specified, each FastQ file's results are written to a corresponding CSV file with each line containing the base position and its average PHRED score.
+
+Examples:
+    python assignment1.py -n 4 sample1.fastq sample2.fastq
+    python assignment1.py -n 8 -o large_dataset.fastq
+
+Author: Mats Slik
+Version: 1.
 """
 # METADATA
 __author__ = "Mats Slik"
-__version__ = "0.6"
+__version__ = "1.0"
 
 
 # Imports
