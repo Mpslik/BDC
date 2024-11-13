897d6a60ad1623f4610e2679760e6f4be72ef4cd
Mats Slik <matsslik5@gmail.com>
2024-11-13 15:59:08 +0100

    first test assignment3.sh

--- Assignment3/assignment3.sh
+++ Assignment3/assignment3.sh
@@ -4,9 +4,8 @@
 
 FASTQ_FILE=""
 
-# Define the number of jobs and block size for parallel processing
-NUM_JOBS=4  # Number of parallel jobs (adjust as needed)
-BLOCK_SIZE=1M  # Block size (adjust based on file size and performance)
+NUM_JOBS=4  # Number of parallel jobs
+BLOCK_SIZE=1M  # Block size
 
 parallel --jobs $NUM_JOBS --pipepart --block $BLOCK_SIZE --sshlogin assemblix2019,nuc109 \
     --recstart '@' \
