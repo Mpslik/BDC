289bb844edfbce07496a06edb03a1dc8ba364ac4
Mats Slik <matsslik5@gmail.com>
2024-12-17 18:55:37 +0100

    updated GNU bash script

--- Assignment3/assignment3.sh
+++ Assignment3/assignment3.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
-# Define the FastQ file to process
+# This script uses GNU parallel to process a FASTQ file in chunks.
+# It relies on the Python script in chunk-parser mode to parse chunks,
+# and then in combine-chunks mode to aggregate results.
 
+# Input FASTQ file
 FASTQ_FILE="/commons/Themas/Thema12/HPC/rnaseq.fastq"
+NUM_JOBS=4
+BLOCK_SIZE=1M
 
-NUM_JOBS=4  # Number of parallel jobs
-BLOCK_SIZE=1M  # Block size
+# Explanation:
+# --jobs $NUM_JOBS: number of parallel jobs
+# --pipepart: process a part of a file in parallel
+# --block $BLOCK_SIZE: size of each chunk
+# --sshlogin: multiple hosts can be specified like host1,host2
+# --recstart '@': each record starts at a line starting with '@'
+# We first run the python script in --chunk-parser mode on each chunk,
+# then pipe all results to the same python script in --combine-chunks mode
+# to produce a final CSV.
 
-parallel --jobs $NUM_JOBS --pipepart --block $BLOCK_SIZE --sshlogin assemblix2019,nuc109 \
+parallel --jobs $NUM_JOBS --pipepart --block $BLOCK_SIZE \
     --recstart '@' \
-    python3 assignment3.py --chunk-parser | \
-    python3 assignment3.py --combine-chunks > output.csv
+    python3 assignment3.py --chunk-parser :::: $FASTQ_FILE \
+    | python3 assignment3.py --combine-chunks > output.csv
