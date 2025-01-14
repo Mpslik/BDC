fb20d7e21cd3965e94b6097f85efd9720291b668
MaDLuck5 <matsslik5@gmail.com>
2025-01-14 12:02:00 +0100

    slurm commands altered

--- Assignment4/assignment4.sh
+++ Assignment4/assignment4.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name=BDC_Assignment4
-#SBATCH --output=assignment4_%j.out
-#SBATCH --error=assignment4_%j.err
 #SBATCH --nodes=1
 #SBATCH --partition=assemblix
 #SBATCH --nodelist=assemblix2019
 #SBATCH --ntasks=5
 #SBATCH --time=00:10:00
 #SBATCH --mem=4G
-#SBATCH --partition=short
 
-
-#module load python/3.11
-#module load mpi4py
 
 # Example arguments to assignment4.py:
 # -n 4 : 4 chunks per file (like assignment1).
